856f60bf8312ad3022f9f25ca134ec801a88876a
Azunn0801 <ngucmnnguoi7@gmail.com>
2026-05-03 22:48:10 +0700

    update day 3

--- Cine-magic.sql
+++ Cine-magic.sql
@@ -50,4 +50,20 @@ DELETE FROM bookings
 WHERE phone = '0987654321';
 
 DELETE FROM movies 
-    WHERE id = 3;
+WHERE id = 3;
+    
+-- Day 3
+SELECT * FROM movies 
+WHERE duration_minutes BETWEEN 90 AND 120;
+
+SELECT * FROM bookings 
+WHERE showtime_id = 2 
+ORDER BY booking_date DESC;
+
+SELECT * FROM movies 
+WHERE age_restriction = 18 OR duration_minutes > 150;
+
+SELECT * FROM showtimes 
+WHERE ticket_price > 100000 
+AND MONTH(show_time) = MONTH(NOW()) 
+AND YEAR(show_time) = YEAR(NOW());
